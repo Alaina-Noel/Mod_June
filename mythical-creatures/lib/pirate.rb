2ad3f33adaf6dc75129e39c7e95226c686d1a365
Alaina Kneiling <alaina.noel.8@gmail.com>
2022-06-17 10:48:41 -0600

    Add a functioning pirate class

---
+++ mythical-creatures/lib/pirate.rb
@@ -0,0 +1,37 @@
+class Pirate
+    attr_reader :name, :job, :cursed, :bad_boy_count
+
+    def initialize(name, job = "Scallywag")
+        @name = name
+        @job = job
+        @cursed = false
+        @bad_boy_count = 0
+    end
+
+    def cursed?
+        if @bad_boy_count > 2
+            @cursed = true
+        else
+            @cursed = false
+        end
+    end
+
+    def commit_heinous_act
+        @bad_boy_count += 1
+    end
+
+end
+
+pirate1 = Pirate.new("Blackbeard", "chef")
+p pirate1.cursed?
+p pirate1.commit_heinous_act
+p pirate1
+p pirate1.commit_heinous_act
+p pirate1
+p pirate1.commit_heinous_act
+p pirate1
+p pirate1.commit_heinous_act
+p pirate1
+p pirate1.commit_heinous_act
+p pirate1.cursed?
+p pirate1
