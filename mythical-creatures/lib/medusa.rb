c8b66ea5fb91c3347f9cf65700ed81dcf1ed7c65
Alaina Kneiling <alaina.noel.8@gmail.com>
2022-06-19 11:23:20 -0600

    Add functioning classes & one fun class called Alien

--- mythical-creatures/lib/medusa.rb
+++ mythical-creatures/lib/medusa.rb
@@ -7,37 +7,58 @@ class Medusa
         @statues = statues
     end
 
-    def stare(victim)
-        puts "hi #{victim}"
-        @statues << victim
-    end
-
-end
-
-
-class Person < Medusa
-    attr_accessor :name
-
-    def initialize(name)
-        @name = name
-    end
-
-    def stoned?
-        if statues.include?(victim)
-            true
-        else
-            false
+    def stare(anyone)
+        if anyone.class == Person
+            puts "hi #{anyone}"
+            @statues << anyone
+            anyone.stoned = true
+        elsif anyone.class == Alien
+            puts "haha i'm an Alien you can't turn me into stone"
         end
     end
 
 end
 
 
-medusa = Medusa.new('Cassiopeia')
-victim = Person.new('Perseus')
-p medusa
-p victim
-# medusa.stare(victim)
-# p statues.class
+class Person
+    attr_accessor :name, :stoned
 
-# victim.stoned?
+    def initialize(name, stoned = false)
+        @name = name
+        @stoned = stoned
+    end
+
+    def stoned?
+        @stoned
+    end
+
+
+end
+
+
+class Alien
+    attr_accessor :name, :stoned
+
+    def initialize(name, stoned = false)
+        @name = name
+        @stoned = stoned
+    end
+
+    def stoned?
+        @stoned
+    end
+
+end
+
+medusa1 = Medusa.new('Cassiopeia')
+ryan = Person.new('Ryan')
+p medusa1
+p ryan
+p medusa1.stare(ryan)
+
+
+#how to resassign
+p medusa1.name = "Pete"
+
+et = Alien.new("ET")
+p medusa1.stare(et)
