d6ec6d9ecba4ee967ce94d43e589ddb7550dc169
Alaina Kneiling <alaina.noel.8@gmail.com>
2022-06-28 11:23:21 -0600

    Add a working eldest method but a broken initialize method

--- command-query/exercises/children.rb
+++ command-query/exercises/children.rb
@@ -1,26 +1,28 @@
-require_relative 'child'
-
 class Children < Array
 
-    # def eldest
-    #     self.max_by {|age| self.age}
-    # end
-
+    def eldest
+        self.max_by {|element| element.age}
+    end
 
 end
 
 class Child
- attr_reader :name, :age
+ attr_accessor :name, :age
 
  def initialize(name, age)
      @name = name
      @age = age
  end
 
+
 end
 
 
-children = Children.new
+children = Children.new("Alexa", 400)
 p children
-children << Child.new("Sarah", 10)
-p children[0].age
+# children << Child.new("Sarah", 10)
+# children << Child.new('Robert', 2)
+# children << Child.new('Fran', 8)
+# children << Child.new('Hilbert', 4)
+# children << Child.new('Hilbert', 40)
+# p children.eldest.name
