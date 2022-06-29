0b6413effb7cba58f6e07b15e0a3fc98ddc0d75e
Alaina Kneiling <alaina.noel.8@gmail.com>
2022-06-29 17:04:49 -0600

    Add my changes

--- command-query/exercises/wallet.rb
+++ command-query/exercises/wallet.rb
@@ -1,25 +1,46 @@
-class Wallet < Array
-    attr_accessor :cents
+require 'pry'
 
-def initialize
-    @cents = 0
-end
+class Wallet
+    attr_accessor :cents, :penny, :dime, :quarter
 
-def penny
-    self << 1
-end
+    def initialize
+        @coins = {
+            :penny => 0,
+            :nickel => 0,
+            :dime => 0,
+            :quarter => 0
+           }
+        @cents = 0
+    end
 
-def cents
-    self.size
-end
+#module that takes a key as an argument
+    def << (penny)
+        @coins[penny] = @coins[penny] + 1
+    end
+
+    def << (nickel)
+        @coins[nickel] = @coins[nickel] + 1
+    end
+
+    def << (dime)
+        @coins[dime] = @coins[dime] + 1
+    end
+
+    def << (quarter)
+        @coins[quarter] = @coins[quarter] + 1
+    end
+
+    def cents
+    sum = 0
+    @coins.each do |k,v|
+      sum += v
+    end
+    return sum
+  end
 
 end
 
 wallet1 = Wallet.new
 p wallet1
-p wallet1.cents
-p wallet1.penny
-p wallet1.penny
-p wallet1.penny
-p wallet1.penny
-p wallet1.cents
+wallet1 << :penny
+p wallet1
